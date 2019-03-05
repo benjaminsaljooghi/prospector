fcd4c7aab7cec51a9bae3ff2806d49c7217f7246
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2019-03-05 22:26:44 +1000

    Add palindrome detection

--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -9,24 +9,19 @@ namespace Parser
 {
     class Program
     {
-        
-
-
-
-        public static void FindCRISPR(string sequence)
+        static void FindCRISPR(string sequence)
         {
 
         }
 
-        public static string Substring(string sequence, int start, int end)
+        static string Substring(string sequence, int start, int end)
         {
             return sequence.Substring(start, end - start);
         }
 
-        public static string ReadFASTA(string file)
+        static string ReadFASTA(string file)
         {
             var reader = new StreamReader(file);
-
             while (true)
             {
                 var line = reader.ReadLine();
@@ -35,16 +30,54 @@ namespace Parser
                     break;
                 }
             }
-
             return reader.ReadToEnd().Replace("\n", "");
+        }
+
+        static string ReverseComplement(string sequence)
+        {
+            char[] chars = sequence.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                switch (chars[i])
+                {
+                    case 'A':
+                        chars[i] = 'T';
+                        break;
+                    case 'C':
+                        chars[i] = 'G';
+                        break;
+                    case 'G':
+                        chars[i] = 'C';
+                        break;
+                    case 'T':
+                        chars[i] = 'A';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return new string(chars);
+        }
+
+        static string Reverse(string sequence)
+        {
+            char[] chars = sequence.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+
+        static bool Palindrome(string sequence)
+        {
+            return Reverse(ReverseComplement(sequence)).Equals(sequence);
         }
 
         static void Main(string[] args)
         {
-            string sequence = ReadFASTA(@"P:\Honours\sequence.fasta");
-            Console.WriteLine("length: {0:n0}", sequence.Length);
-            string cas9 = Substring(sequence, 854751, 858857);
-            Console.WriteLine("cas9, len: " + cas9.Length + "\n" + cas9);
+            Console.WriteLine(Palindrome("ACCTAGGT"));
+            //string sequence = ReadFASTA(@"P:\Honours\sequence.fasta");
+            //Console.WriteLine("length: {0:n0}", sequence.Length);
+            //string cas9 = Substring(sequence, 854751, 858857);
+            //Console.WriteLine("cas9, len: " + cas9.Length + "\n" + cas9);
             //string crispr = Substring(sequence, 860819, 861250);
             //Console.WriteLine("cas9: ", cas9);
             //Console.WriteLine("\ncrispr: ", crispr);
