71112d63c3282a85753ba4c9da4fbe01abd8adb6
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2019-03-05 22:09:09 +1000

    Some initial code and data

---
+++ Parser/Parser/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    class Program
+    {
+        
+
+
+
+        public static void FindCRISPR(string sequence)
+        {
+
+        }
+
+        public static string Substring(string sequence, int start, int end)
+        {
+            return sequence.Substring(start, end - start);
+        }
+
+        public static string ReadFASTA(string file)
+        {
+            var reader = new StreamReader(file);
+
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line.StartsWith(">"))
+                {
+                    break;
+                }
+            }
+
+            return reader.ReadToEnd().Replace("\n", "");
+        }
+
+        static void Main(string[] args)
+        {
+            string sequence = ReadFASTA(@"P:\Honours\sequence.fasta");
+            Console.WriteLine("length: {0:n0}", sequence.Length);
+            string cas9 = Substring(sequence, 854751, 858857);
+            Console.WriteLine("cas9, len: " + cas9.Length + "\n" + cas9);
+            //string crispr = Substring(sequence, 860819, 861250);
+            //Console.WriteLine("cas9: ", cas9);
+            //Console.WriteLine("\ncrispr: ", crispr);
+
+        }
+    }
+}
