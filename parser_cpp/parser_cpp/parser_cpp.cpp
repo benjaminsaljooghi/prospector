4666135f52ebcb505c383d8d97c45777bb0fa65f
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2019-05-30 10:50:33 +1000

    Update parser_cpp.cpp

--- parser_cpp/parser_cpp/parser_cpp.cpp
+++ parser_cpp/parser_cpp/parser_cpp.cpp
@@ -7,21 +7,19 @@ using namespace std;
 
 map<string,string> parse_fasta(string file_path)
 {
+    
     cout << "reading: " << file_path << endl;
-
-    map<string, string> seqs;
-
     ifstream input(file_path);
     if (!input.good()) {
         throw "Error opening " + file_path;
     }
 
+    map<string, string> seqs;
     string line, name, content;
     while (getline(input, line)) {
 
         if (line.empty() || line[0] == '>') { // Identifier marker
-            if (!name.empty()) { // Print out what we read from the last entry
-                //final_out += name + " : " + content + "\n";
+            if (!name.empty()) { // Get what we read from the last entry
                 seqs[name] = content;
                 name.clear();
             }
@@ -31,8 +29,6 @@ map<string,string> parse_fasta(string file_path)
             content.clear();
         }
         else if (!name.empty()) {
-
-
             if (line.find(' ') != string::npos) { // Invalid sequence--no spaces allowed
                 name.clear();
                 content.clear();
@@ -42,7 +38,7 @@ map<string,string> parse_fasta(string file_path)
             }
         }
     }
-    if (!name.empty()) { // Print out what we read from the last 
+    if (!name.empty()) { // Get what we read from the last 
         seqs[name] = content;
     }
 
@@ -60,7 +56,8 @@ int main()
 {
     string test_path = R"(P:\CRISPR\test_data\test.fasta)";
     string aureus_path = R"(P:\CRISPR\bacteria\aureus.fasta)";
-    string out = parse_single_seq(aureus_path);       
-    cout << "the content is" << endl << out;
+    string aureus = parse_single_seq(aureus_path);       
+    
+    
     return 0;
 }
