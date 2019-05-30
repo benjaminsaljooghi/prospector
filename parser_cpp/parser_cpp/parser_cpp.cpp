6f4b224597988df95c94c7e480d743c96f6ef5ca
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2019-05-30 10:49:08 +1000

    C++ map-based fasta parsing, and single seq parsing

--- parser_cpp/parser_cpp/parser_cpp.cpp
+++ parser_cpp/parser_cpp/parser_cpp.cpp
@@ -1,22 +1,19 @@
-// parser_cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 
 using namespace std;
 
-string parse_fasta(string file_path)
+map<string,string> parse_fasta(string file_path)
 {
-    cout << "reading" << ++file_path << endl;
+    cout << "reading: " << file_path << endl;
 
-    string final_out = "";
+    map<string, string> seqs;
 
     ifstream input(file_path);
     if (!input.good()) {
-        cerr << "Error opening '" << file_path << "'. Bailing out." << endl;
-        return "";
+        throw "Error opening " + file_path;
     }
 
     string line, name, content;
@@ -24,7 +21,8 @@ string parse_fasta(string file_path)
 
         if (line.empty() || line[0] == '>') { // Identifier marker
             if (!name.empty()) { // Print out what we read from the last entry
-                final_out += name + " : " + content + "\n";
+                //final_out += name + " : " + content + "\n";
+                seqs[name] = content;
                 name.clear();
             }
             if (!line.empty()) {
@@ -45,24 +43,24 @@ string parse_fasta(string file_path)
         }
     }
     if (!name.empty()) { // Print out what we read from the last 
-        final_out += name + " : " + content + "\n";
+        seqs[name] = content;
     }
 
-    return final_out;
+    return seqs;
 }
 
-int main(int argc, char** argv)
+string parse_single_seq(string file_path)
 {
+    map<string, string> seqs = parse_fasta(file_path);
+    string seq = seqs.begin()->second;
+    return seq;
+}
 
-
-    if (argc <= 1) {
-        cerr << "Usage: " << argv[0] << " [infile]" << endl;
-        return -1;
-    }
-
-    string out = parse_fasta(argv[1]);
-        
-    cout << out;
-
+int main()
+{
+    string test_path = R"(P:\CRISPR\test_data\test.fasta)";
+    string aureus_path = R"(P:\CRISPR\bacteria\aureus.fasta)";
+    string out = parse_single_seq(aureus_path);       
+    cout << "the content is" << endl << out;
     return 0;
 }
