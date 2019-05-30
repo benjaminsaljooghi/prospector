909a8cbce65fb46e95c2077401688f0bbba3cca6
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2019-05-30 11:53:58 +1000

    kmer generation, dyad analysis

--- parser_cpp/parser_cpp/parser_cpp.cpp
+++ parser_cpp/parser_cpp/parser_cpp.cpp
@@ -1,44 +1,77 @@
+using namespace std;
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <map>
+#include <vector>
 
-using namespace std;
+#define DYAD_MIN 5
 
-map<string,string> parse_fasta(string file_path)
+//const map<char, char> complements =
+//{
+//    { 'A', 'T' },
+//    { 'T', 'A' },
+//    { 'C', 'G' },
+//    { 'G', 'C' },
+//    { 'N', 'N' },
+//    { 'n', 'n' },
+//};
+
+const map<char, char> complements =
 {
-    
+    { 'A', 'T' },
+    { 'T', 'A' },
+    { 'C', 'G' },
+    { 'G', 'C' },
+    { 'N', 'N' },
+    { 'n', 'n' },
+};
+
+map<string, string> parse_fasta(string file_path)
+{
+
     cout << "reading: " << file_path << endl;
     ifstream input(file_path);
-    if (!input.good()) {
+    if (!input.good())
+    {
         throw "Error opening " + file_path;
     }
 
     map<string, string> seqs;
     string line, name, content;
-    while (getline(input, line)) {
-
-        if (line.empty() || line[0] == '>') { // Identifier marker
-            if (!name.empty()) { // Get what we read from the last entry
+    while (getline(input, line))
+    {
+        if (line.empty() || line[0] == '>') // Identifier marker
+        { 
+            if (!name.empty())
+            { 
+                // Get what we read from the last entry
                 seqs[name] = content;
                 name.clear();
             }
-            if (!line.empty()) {
+            if (!line.empty())
+            {
                 name = line.substr(1);
             }
             content.clear();
         }
-        else if (!name.empty()) {
-            if (line.find(' ') != string::npos) { // Invalid sequence--no spaces allowed
+        else if (!name.empty())
+        {
+            if (line.find(' ') != string::npos) // Invalid sequence--no spaces allowed
+            { 
                 name.clear();
                 content.clear();
             }
-            else {
+            else
+            {
                 content += line;
             }
         }
     }
-    if (!name.empty()) { // Get what we read from the last 
+    if (!name.empty())
+    { 
+        // Get what we read from the last 
         seqs[name] = content;
     }
 
@@ -52,12 +85,50 @@ string parse_single_seq(string file_path)
     return seq;
 }
 
+vector<string> generate_kmers(string sequence, int k)
+{
+    vector<string> seqs;
+
+    for (size_t i = 0; i < sequence.length() - k + 1; i++)
+    {
+        seqs.push_back(sequence.substr(i, k));
+    }
+
+    return seqs;
+}
+
+bool is_dyad(string seq)
+{
+    int len = seq.length();
+    for (int i = 0; i < DYAD_MIN; i++)
+    {
+        char beginning_upstream = seq[i];
+        char end_downstream = seq[len - i - 1];
+        char end_downstream_comp = complements.at(end_downstream);
+        if (beginning_upstream != end_downstream_comp)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void discover_crisprs(string genome, vector<string> kmers)
+{
+
+}
+
 int main()
 {
+
     string test_path = R"(P:\CRISPR\test_data\test.fasta)";
     string aureus_path = R"(P:\CRISPR\bacteria\aureus.fasta)";
-    string aureus = parse_single_seq(aureus_path);       
-    
-    
+
+    string aureus = parse_single_seq(aureus_path);
+    cout << aureus.length() << endl;
+
+    vector<string> kmers = generate_kmers(aureus, 6);
+    cout << kmers.size() << endl;
+
     return 0;
 }
