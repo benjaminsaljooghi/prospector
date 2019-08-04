9fde8e01150e654e83e726d5665e06febc0bc524
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2019-08-04 10:03:02 +1000

    Extract more utility functions from prospector.cu to util.h

--- prospector/prospector.cu
+++ prospector/prospector.cu
@@ -1,22 +1,4 @@
-using namespace std;
-
-// CUDA
-#include "cuda.h"
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include "cuda_fp16.h"
-
-#ifdef __CUDACC__
-#define CUDA_CALLABLE_MEMBER __host__ __device__
-#define KERNEL_ARGS2(grid, block) <<< grid, block >>>
-#define KERNEL_ARGS3(grid, block, sh_mem) <<< grid, block, sh_mem >>>
-#define KERNEL_ARGS4(grid, block, sh_mem, stream) <<< grid, block, sh_mem, stream >>>
-#else
-#define CUDA_CALLABLE_MEMBER
-#define KERNEL_ARGS2(grid, block)
-#define KERNEL_ARGS3(grid, block, sh_mem)
-#define KERNEL_ARGS4(grid, block, sh_mem, stream)
-#endif
+#include "util.h"
 
 #define DYAD_MIN 5
 #define REPEAT_MIN 20
@@ -32,8 +14,6 @@ using namespace std;
 #define K_START 20
 #define K_END 60
 #define BUFFER 10
-
-#include "util.h"
 
 __device__ char complement(char nuc)
 {
@@ -148,99 +128,13 @@ vector<int> dyad_lengths(vector<vector<int>> all_dyads)
     return lengths;
 }
 
-vector<int> flatten(vector<vector<int>> all_dyads)
-{
-    vector<int> flattened;
-    for (auto v : all_dyads)
-        flattened.insert(flattened.end(), v.begin(), v.end());
-    return flattened;
-}
-
-
 int* create_buffer(int count)
 {
-    int* buffer = (int*)malloc(count * sizeof(int));
-    for (int i = 0; i < count; i++)
-        buffer[i] = -1;
-    return buffer;
+	int* buffer = (int*)malloc(count * sizeof(int));
+	for (int i = 0; i < count; i++)
+		buffer[i] = -1;
+	return buffer;
 }
-
-bool vec_contains(vector<int> a, vector<int> b)
-{
-    for (auto a_elt : a)
-    {
-        if (find(b.begin(), b.end(), a_elt) == b.end())
-            return false;
-    }
-    return true;
-}
-
-void cfree(void* device_ptr)
-{
-    printf("executing cudafree\n");
-    cudaError err = cudaFree(device_ptr);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "failed to free device ptr (error code %s)!\n", cudaGetErrorString(err));
-        exit(err);
-    }
-}
-
-template <typename T> void pull(T* h, const T* d, int count)
-{
-    size_t bytes = count * sizeof(T);
-
-    cudaError err;
-
-    printf("memcpy %*d bytes from device...\n", PRINTF_BYTE_FORMAT_ALIGN, bytes);
-    err = cudaMemcpy(h, d, bytes, cudaMemcpyDeviceToHost);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "failed to copy from device to host (error code %s)!\n", cudaGetErrorString(err));
-        exit(err);
-    }
-}
-
-template <typename T> T* push(const T* src, int count)
-{
-
-    size_t bytes = count * sizeof(T);
-
-    cudaError err;
-    T* ptr = NULL;
-    
-    printf("malloc %*d bytes on device...\n", PRINTF_BYTE_FORMAT_ALIGN, bytes);
-    err = cudaMalloc((void**)& ptr, bytes);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "failed to malloc device (error code %s)!\n", cudaGetErrorString(err));
-        exit(err);
-    }
-
-    printf("memcpy %*d bytes to device...\n", PRINTF_BYTE_FORMAT_ALIGN, bytes);
-    err = cudaMemcpy(ptr, src, bytes, cudaMemcpyHostToDevice);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "failed to copy from host to device (error code %s)!\n", cudaGetErrorString(err));
-        exit(err);
-    }
-
-    return (T*)ptr;
-}
-
-void wait_cuda()
-{
-    printf("waiting for kernel... ");
-    BEGIN;
-    cudaError err = cudaDeviceSynchronize();
-    END;
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Cuda error in file '%s' in line %i : %s.\n", __FILE__, __LINE__, cudaGetErrorString(err));
-        exit(err);
-    }
-}
-
 
 vector<vector<int>> dyad_gen(char* device_genome, size_t genome_len, int k_start, int k_end)
 {
@@ -273,8 +167,6 @@ vector<vector<int>> dyad_gen(char* device_genome, size_t genome_len, int k_start
 
     return all_dyads;
 }
-
-
 
 vector<vector<int>> crispr_gen(char* device_genome, size_t genome_len, int k_start, int k_end, int min_repeats, int buffer_size, vector<vector<int>> all_dyads)
 {
@@ -342,10 +234,9 @@ vector<vector<int>> crispr_gen(char* device_genome, size_t genome_len, int k_sta
     return vec_crisprs;
 }
 
-
 void run(string genome_path, int min_repeats, int k_start, int k_end, int buffer_size)
 {
-    string genome_str = parse_genome(genome_path);
+    string genome_str = parse_single_seq(genome_path);
     const char* genome = genome_str.c_str();
     size_t genome_len = strlen(genome);
     char* device_genome = push(genome, genome_len);
@@ -369,7 +260,6 @@ void run(string genome_path, int min_repeats, int k_start, int k_end, int buffer
         }
         printf("%d %d:\t%s\n", k, vec[1], crispr_str.c_str());
     }
-
 }
 
 string get_genome_path(char** argv)
--- prospector/util.h
+++ prospector/util.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// std
+// C++
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -15,13 +15,28 @@
 #include <set>
 #include <ctime>
 #include <string>
-
-// My stuff
-//#include "Sequence.h"
-
 using namespace std;
 
-// Timing
+// CUDA
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "cuda_fp16.h"
+
+// Macros
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#define KERNEL_ARGS2(grid, block) <<< grid, block >>>
+#define KERNEL_ARGS3(grid, block, sh_mem) <<< grid, block, sh_mem >>>
+#define KERNEL_ARGS4(grid, block, sh_mem, stream) <<< grid, block, sh_mem, stream >>>
+#else
+#define CUDA_CALLABLE_MEMBER
+#define KERNEL_ARGS2(grid, block)
+#define KERNEL_ARGS3(grid, block, sh_mem)
+#define KERNEL_ARGS4(grid, block, sh_mem, stream)
+#endif
+
+#pragma region Timing
 
 clock_t start;
 #define BEGIN start = clock();
@@ -32,7 +47,9 @@ double duration(clock_t begin)
 	return (clock() - begin) / (double)CLOCKS_PER_SEC;
 }
 
+#pragma endregion
 
+#pragma region Fasta
 
 map<string, string> parse_fasta(string file_path)
 {
@@ -88,15 +105,101 @@ string parse_single_seq(string file_path)
 {
 	map<string, string> seqs = parse_fasta(file_path);
 	string seq = seqs.begin()->second;
-	//return Sequence(seq, 0);
 	return seq;
 }
 
+#pragma endregion
 
-string parse_genome(string file_path)
+#pragma region Vectors
+
+vector<int> flatten(vector<vector<int>> vecs)
 {
-	printf("parse genome...\n");
-	//string genome = parse_single_seq(file_path).seq;
-	//return genome;
-	return parse_single_seq(file_path);
+	vector<int> flattened;
+	for (auto v : vecs)
+		flattened.insert(flattened.end(), v.begin(), v.end());
+	return flattened;
 }
+
+bool vec_contains(vector<int> a, vector<int> b)
+{
+	for (auto a_elt : a)
+	{
+		if (find(b.begin(), b.end(), a_elt) == b.end())
+			return false;
+	}
+	return true;
+}
+
+#pragma endregion
+
+#pragma region CUDA
+
+
+void cfree(void* device_ptr)
+{
+	printf("executing cudafree\n");
+	cudaError err = cudaFree(device_ptr);
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "failed to free device ptr (error code %s)!\n", cudaGetErrorString(err));
+		exit(err);
+	}
+}
+
+template <typename T> void pull(T* h, const T* d, int count)
+{
+	size_t bytes = count * sizeof(T);
+
+	cudaError err;
+
+	printf("memcpy %*d bytes from device...\n", PRINTF_BYTE_FORMAT_ALIGN, bytes);
+	err = cudaMemcpy(h, d, bytes, cudaMemcpyDeviceToHost);
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "failed to copy from device to host (error code %s)!\n", cudaGetErrorString(err));
+		exit(err);
+	}
+}
+
+template <typename T> T* push(const T* src, int count)
+{
+
+	size_t bytes = count * sizeof(T);
+
+	cudaError err;
+	T* ptr = NULL;
+
+	printf("malloc %*d bytes on device...\n", PRINTF_BYTE_FORMAT_ALIGN, bytes);
+	err = cudaMalloc((void**)& ptr, bytes);
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "failed to malloc device (error code %s)!\n", cudaGetErrorString(err));
+		exit(err);
+	}
+
+	printf("memcpy %*d bytes to device...\n", PRINTF_BYTE_FORMAT_ALIGN, bytes);
+	err = cudaMemcpy(ptr, src, bytes, cudaMemcpyHostToDevice);
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "failed to copy from host to device (error code %s)!\n", cudaGetErrorString(err));
+		exit(err);
+	}
+
+	return (T*)ptr;
+}
+
+void wait_cuda()
+{
+	printf("waiting for kernel... ");
+	BEGIN;
+	cudaError err = cudaDeviceSynchronize();
+	END;
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "Cuda error in file '%s' in line %i : %s.\n", __FILE__, __LINE__, cudaGetErrorString(err));
+		exit(err);
+	}
+}
+
+
+#pragma endregion
